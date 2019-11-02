3dc78f66630267a8ab248757d1fa56b3d8d97032
Jack Bates <jack@nottheoilrig.com>
2019-11-02 14:18:51 -0700

    Defer generic awaited type

--- src/lib/es2018.asyncgenerator.d.ts
+++ src/lib/es2018.asyncgenerator.d.ts
@@ -2,9 +2,9 @@
 
 interface AsyncGenerator<T = unknown, TReturn = any, TNext = unknown> extends AsyncIterator<T, TReturn, TNext> {
     // NOTE: 'next' is defined using a tuple to ensure we report the correct assignability errors in all places.
-    next(...args: [] | [TNext]): Promise<IteratorResult<T, TReturn>>;
-    return(value: TReturn | PromiseLike<TReturn>): Promise<IteratorResult<T, TReturn>>;
-    throw(e: any): Promise<IteratorResult<T, TReturn>>;
+    next(...args: [] | [TNext]): Promise<IteratorResult<T, Awaited<TReturn>>>;
+    return(value: TReturn): Promise<IteratorResult<T, Awaited<TReturn>>>;
+    throw(e: any): Promise<IteratorResult<T, Awaited<TReturn>>>;
     [Symbol.asyncIterator](): AsyncGenerator<T, TReturn, TNext>;
 }
 
